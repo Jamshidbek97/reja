9f2acec13f008f985bd434a8d25aa7ea02685d20
Jamshidbek97 <urologiya@internet.ru>
2025-02-21 04:40:29 +0900

    feat: develop autor router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web serverni boshlash");
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("Error", err); 
+    } else {
+    user = JSON.parse(data);
+    }
+});
 
 // 1 starting codes
 app.use(express.static('public'));
@@ -23,6 +33,10 @@ app.post("/create-item", (req, res) =>{
 
 app.get("/", function(req, res){
     res.render("harid");
+});
+
+app.get("/author", (req, res) =>{
+    res.render("author", {user: user});
 });
 
 const server = http.createServer(app);
