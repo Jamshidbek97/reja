a4a705df8870f8c65b6a2e0b360c045e05f4b314
Jamshidbek97 <urologiya@internet.ru>
2025-02-26 00:30:49 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 const app = express();
 // 1 express kirish code
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 //2 sessions code
@@ -26,22 +28,26 @@ app.set("view engine", "ejs");
 //4 routing code
 //form in harid has action sending it to /create-item
 app.post("/create-item", (req, res) => {
-  res.json({ test: "success" });
+  res.json({
+    test: "success"
+  });
 });
 
 ///main page rendering harid.ejs in views
 app.get("/", (req, res) => {
-  res.render("harid");
+  res.render("reja");
 });
 
 //// author page route
 app.get("/author", (req, res) => {
-  res.render("author", { user: user });
+  res.render("author", {
+    user: user
+  });
 });
 
 //creating server with https
 let PORT = 3000;
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log(`this app is running in port: ${PORT}`);
+  console.log(`this app is running in port: ${PORT},n https://localhost:${PORT}`);
 });
